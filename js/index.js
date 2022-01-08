630fc7e81a3a38a7c4ac9976af97392fbd457961
sewprano <sopranosamurai@yahoo.com>
2022-01-07 20:21:09 -0800

    start over

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const copyrightyear = document.getElementById("copyrightyear");
-copyrightyear.innerHTML = new Date().getFullYear();
-
-//let datemodified = new Date(document.lastModified);
-//document.getElementById(modified).innerHTML;
-
-let datemodified = document.getElementById("modified");
-modified.innerHTML = new Date(document.lastModified);
-//document.getElementById("modified").innerHTML = dateModified;
