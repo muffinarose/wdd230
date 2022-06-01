a4b106a6c52986e089a1cf6ca28901509012b28e
sewprano <sopranosamurai@yahoo.com>
2022-06-01 11:04:27 -0700

    Responsive menu & section names

---
+++ Practice/Hamburger Menu/js/scripts.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamButton").classList.toggle("open");
+}
+const x = document.getElementById("hamButton");
+x.onclick = toggleMenu;
