58f400f5d9f1f0dd4ff7aa67a21ff091a6679744
sewprano <sopranosamurai@yahoo.com>
2022-06-11 21:55:59 -0700

    Header alignment and final fixes wk 4

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,7 +1,8 @@
 // Hamburger Menu Toggle
 function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamButton").classList.toggle("open");
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamButton").classList.toggle("open");
+  document.getElementById("main-nav").classList.toggle("open");
 }
 
 const x = document.getElementById("hamButton");
@@ -9,32 +10,49 @@ x.onclick = toggleMenu;
 
 // Day of the Week
 function isMondayTuesday() {
-    if (new Date().getDay() == 1 || new Date().getDay() == 2){
-        return true; }
-    else {
-        return false; }
-} 
+  if (new Date().getDay() == 1 || new Date().getDay() == 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
 //full Date
 
 let date = new Date();
 
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", 
-                    "Thursday", "Friday", "Saturday"];
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 let dayOfWeek = days[date.getDay()];
 
-let dayOfMonth =  [date.getDate()];
+let dayOfMonth = [date.getDate()];
 
-const months = ["January", "February", "March", "April", "May", 
-                "June","July", "August", "September", "October", 
-                "November", "December"];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 let month = months[date.getMonth()];
 
 let year = [date.getFullYear()];
- 
-document.getElementById("header-date").innerHTML = 
-        dayOfWeek + ", " + dayOfMonth + " " + month + " " + year; 
 
-
+document.getElementById("header-date").innerHTML =
+  dayOfWeek + ", " + dayOfMonth + " " + month + " " + year;
 
 //year
 const copyrightyear = document.getElementById("copyrightyear");
@@ -46,7 +64,7 @@ modified.innerHTML = new Date(document.lastModified);
 
 //Mon Tues Banner
 if (isMondayTuesday()) {
-    document.getElementById("mon-tues-banner").style.display = "block";
+  document.getElementById("mon-tues-banner").style.display = "block";
 }
 //troubleshooting tests:
 
